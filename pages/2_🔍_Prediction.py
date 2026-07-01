ad7b26181e1dcc5f267bb1dc6288b2c93d738849
Nandini Agarwal <nandiniagarwal@Nandinis-Laptop.Dlink>
2026-07-01 16:00:40 +0530

    Reorganize project structure and update dependencies

--- pages/2_🔍_Prediction.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("🔍 Customer Prediction")
-
-st.write("Predict whether an individual customer will churn.")
-
-st.info("Prediction form will be added in Module 3.")
---
+++ src/predict.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def prepare_input(customer_data):
+
+    df = pd.DataFrame([customer_data])
+
+    return df
